a714fb9db6e7a5b15606a4fa4bfebd0a7345f9cd
Anderson Dias <olhardeveloper@gmail.com>
2023-04-05 16:39:53 -0300

    feat: criando rotas e http status codes

---
+++ src/server/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+
+const router = Router();
+
+router.get('/',(req, res) => { 
+    return res.send('olá dev');
+});
+
+router.post('/teste' ,(req, res) => { 
+    console.log(req.body);
+    return res.status( StatusCodes.UNAUTHORIZED).send('rota post');
+});
+
+export{ router };
